ede2f2ec7046cf94f6f6d0d4f95f5b4c16fa6fc1
Sneha KSJ <110806539+Snehaksj@users.noreply.github.com>
2023-08-29 11:33:22 +0530

    Update 2.4 udp_chat_server.py

--- expt2/2.4 udp_chat_server.py
+++ expt2/2.4 udp_chat_server.py
@@ -1,16 +1,5 @@
 import socket
 
-def calculate(l):
-    if l[2]=='+':
-        return str(int(l[0])+int(l[1]))
-    elif l[2]=='-':
-        return str(int(l[0])-int(l[1]))
-    elif l[2]=='*':
-        return str(int(l[0])*int(l[1]))
-    elif l[2]=='/':
-        return str(int(l[0])/int(l[1]))
-    
-
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 server_socket.bind(('', 12000))
@@ -19,7 +8,14 @@ while True:
 
     data, address =server_socket.recvfrom(1024)
     message = data.decode('utf-8')
-    print("Received message from client...")
-    newmessage=calculate(message)
-    print("Sending message to client...")
-    server_socket.sendto(newmessage.encode('utf-8'),address)
+    print("Recieved from client...")
+    if message=='bye':
+        print('bye')
+        break
+    else:
+        print(message)
+    msg= input("Enter message")
+    print("sending message to client")
+    server_socket.sendto(msg.encode('utf-8'),address)
+    if(msg=='bye'):
+        break
