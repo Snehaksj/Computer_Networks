ab45203bed38780ed50a779af071f13ef6663218
Sneha KSJ <110806539+Snehaksj@users.noreply.github.com>
2023-08-29 11:33:00 +0530

    Update 2.4 udp_chat_client.py

--- expt2/2.4 udp_chat_client.py
+++ expt2/2.4 udp_chat_client.py
@@ -1,15 +1,19 @@
 import socket
 
 client_socket =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-message = 'test'
-
-print("Sending message to server...")
-
 addr = ("127.0.0.1", 12000)
 
-client_socket.sendto(message.encode('utf-8'), addr)
+while True:
+    string=input("Enter message:")
+    
+    print("Sending message to server...")
 
-data, server =client_socket.recvfrom(1024)
-response = data.decode('utf-8')
-print("Receieved message from server:",response)
+    client_socket.sendto(string.encode('utf-8'), addr)
+    if(string=='bye'):
+        break
+    
+    data, server =client_socket.recvfrom(1024)
+    response = data.decode('utf-8')
+    print("Receieved message from server:",response)
+    if response=='bye':
+        break
